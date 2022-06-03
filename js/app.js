dcd389d2e80af1163d44fbaa0c04c0cdccb2c63f
Pedro Arturo <58676472+Pedri0@users.noreply.github.com>
2022-06-02 19:25:17 -0500

    add more buttons with diferent js functions

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function alertFunc(){
 }
 
 function userConfirm(){
-    let confirmation = confirm("You clicked the button Are You sure?")
+    let confirmation = confirm("You clicked the button Are You sure?");
 
     if(confirmation){
         return alert("You Pressed OK C;")
@@ -11,4 +11,25 @@ function userConfirm(){
     else{
         return alert("You Pressed Cancel :C")
     }
+}
+
+function changeColor(){
+    let divElement = document.getElementById("extraDiv");
+
+    divElement.className = "whiteDiv";
+}
+
+function returnColor(){
+    let divElement = document.getElementById("extraDiv");
+    divElement.className = "leftDiv";
+}
+
+function changeText(){
+    let divElement = document.getElementById("textDiv");
+    divElement.innerHTML = "Cleaned :c"
+}
+
+function changeBtn(){
+    let divElement = document.getElementById("textBtn");
+    divElement.value = "You changed the text in left button"
 }
